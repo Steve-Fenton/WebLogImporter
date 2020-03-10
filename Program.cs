2de6c63d14eece34a3ab2b421f1d0b7c34c1dcfe
Steve Fenton <Steve.Fenton@GeronimoWeb.com>
2020-03-10 16:01:10 +0000

    First draft - works, but not yet beautiful.

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace Fenton.WebLogImporter
+{
+    class Program
+    {
+        private static readonly string _in = "c:\\Temp\\Logs\\In";
+        private static readonly string _out = "c:\\Temp\\Logs\\Out";
+        private static readonly string _outFile = "c:\\Temp\\Logs\\Out\\out.log";
+        private static readonly string _headerStart = "#";
+        private static readonly string _fieldStart = "#Fields:";
+        private static readonly string _db = "Server=.;Database=WebLogs;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        static void Main(string[] args)
+        {
+            ConfigureFileSystem();
+
+            CombineAndCleanseFiles();
+
+            EmptyTable();
+
+            BulkLoadData();
+        }
+
+        private static void CombineAndCleanseFiles()
+        {
+            bool headerRowProcessed = false;
+
+            using (StreamWriter outfile = new StreamWriter(File.Create(_outFile)))
+            {
+                foreach (var file in Directory.GetFiles(_in))
+                {
+                    Console.WriteLine(file);
+
+                    using FileStream fileStream = new FileStream(file, FileMode.Open);
+                    using StreamReader reader = new StreamReader(fileStream);
+
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        if (line.StartsWith(_headerStart))
+                        {
+                            if (!headerRowProcessed && line.StartsWith(_fieldStart))
+                            {
+                                headerRowProcessed = true;
+                                outfile.WriteLine(line);
+                            }
+                        }
+                        else
+                        {
+                            outfile.WriteLine(line);
+                        }
+
+                        line = reader.ReadLine();
+                    }
+                }
+
+                outfile.Close();
+            }
+        }
+
+        [SuppressMessage("Security", "CA2100:Review SQL queries for security vulnerabilities", Justification = "The _outFile item is not user defined.")]
+        private static void BulkLoadData()
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                using (var command = new SqlCommand()
+                {
+                    CommandText = "BULK INSERT LogEntry FROM '" + _outFile + "' WITH (FIRSTROW = 2, FIELDTERMINATOR = ' ', ROWTERMINATOR = '\n')",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                })
+                {
+                    connection.Open();
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private static void EmptyTable()
+        {
+            using (var connection = new SqlConnection(_db))
+            {
+                using (var command = new SqlCommand()
+                {
+                    CommandText = "TRUNCATE TABLE LogEntry",
+                    CommandType = CommandType.Text,
+                    Connection = connection
+                })
+                {
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private static void ConfigureFileSystem()
+        {
+            IList<string> directories = new List<string>
+            {
+                "c:\\Temp",
+                "c:\\Temp\\Logs",
+                _in,
+                _out,
+            };
+
+            foreach (var directory in directories)
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            if (File.Exists(_outFile))
+            {
+                File.Delete(_outFile);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,112 @@
+﻿# Web Log Importer
+
+This app will read all files placed in the following directory, combine them and strip unnecessary headers, and bulk load them into SQL.
+
+    C:\Temp\Logs\In
+
+# One Time Set Up
+
+ - Create a couple of folders... or let the app do it
+ - Create a database with a table (script below)
+
+To start with, you'll need the following folders:
+
+    C:\Temp\Logs\In
+    C:\Temp\Logs\Out
+
+Running the app for the first time will create these folders... you just need to remember to use them!
+
+And the following SQL database:
+
+	USE [master]
+	GO
+
+	CREATE DATABASE [WebLogs]
+	GO
+	ALTER DATABASE [WebLogs] SET RECOVERY SIMPLE 
+	GO
+
+	USE [WebLogs]
+	GO
+
+	SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+
+	CREATE TABLE [dbo].[LogEntry](
+		[date] datetime2 NULL,
+		[time] datetime2 NULL,
+		[s_ip] [nvarchar](50) NULL,
+		[cs_method] [nvarchar](50) NULL,
+		[cs_uri_stem] [nvarchar](max) NULL,
+		[cs_uri_query] [nvarchar](max) NULL,
+		[s_port] [int] NULL,
+		[cs_username] [nvarchar](max) NULL,
+		[c_ip] [nvarchar](50) NULL,
+		[cs_User_Agent_] [nvarchar](max) NULL,
+		[cs_Referer_] [nvarchar](max) NULL,
+		[cs_host] [nvarchar](max) NULL,
+		[sc_status] [int] NULL,
+		[sc_substatus] [int] NULL,
+		[sc_win32_status] [int] NULL,
+		[time_taken] [int] NULL,
+		[X_Forwarded_For] [nvarchar](50) NULL,
+		INDEX cci CLUSTERED COLUMNSTORE
+	) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+	GO
+
+# Database Access
+
+To keep things simple, the user running the app will need permissions on the database.
+
+# Running the App
+
+Fill the "in" folder with IIS logs, then just run the app.
+
+    > Fenton.WebLogImporter.exe
+
+# Queries
+
+It's just SQL queries now... so knock yourself out...
+
+## X-Forwarded-For Rankings
+
+	SELECT
+		[X_Forwarded_For],
+		COUNT(1)
+	FROM
+		LogEntry
+	GROUP BY
+		[X_Forwarded_For]
+	ORDER BY
+		COUNT(1) DESC
+
+## Host Rankings
+
+	SELECT
+		[cs_host], COUNT(1)
+	FROM
+		LogEntry
+	GROUP BY
+		[cs_host]
+	ORDER BY
+		COUNT(1) DESC
+
+## Errors
+
+	SELECT
+		*
+	FROM
+		LogEntry
+	WHERE
+		[sc_status] >= 400
+
+## Filter by Date and Time
+
+You can filter by date and time using the following (using "date =" because time is in a separate column, and "time BETWEEN" to grab a range)
+
+	WHERE
+		[date] = '2020-03-10'
+	AND
+		[time] BETWEEN '12:20' AND '12:50'
